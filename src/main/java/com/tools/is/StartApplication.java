9b9fe4b85818bd11519d8cce290409a241ee1555
yyulb <15032356760@163.com>
2020-12-16 09:35:17 +0800

    feature 项目结构

--- src/main/java/com/tools/is/StartApplication.java
+++ src/main/java/com/tools/is/StartApplication.java
@@ -1,5 +1,13 @@
 package com.tools.is;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class StartApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
 }
