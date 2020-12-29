bc2f3dce3e05e2651387e8f35b2528874b456492
yyulb <15032356760@163.com>
2020-12-29 14:10:12 +0800

    fix 添加zookeeper操作类

--- src/test/java/com/tools/is/TestZk.java
+++ src/test/java/com/tools/is/TestZk.java
@@ -1,20 +1,27 @@
 package com.tools.is;
 
-import com.tools.is.utils.SnowflakeIdUtil;
+import com.tools.is.service.ZookeeperService;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.zookeeper.CreateMode;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
+/**
+ *测试操作zk
+ */
 @SpringBootTest(classes = StartApplication.class)
 @RunWith(SpringRunner.class)
 @Slf4j
-public class TestAll {
+public class TestZk {
+
+    @Autowired
+    private ZookeeperService zookeeperService;
 
     @Test
     public void test(){
-        System.out.println(SnowflakeIdUtil.nextId());
+        zookeeperService.createNodeAndData(CreateMode.PERSISTENT,"testzk","test");
     }
-
 }
